92d2afb5aa66766ec9c19b3ec844e9ffab65a201
Norm Matloff <matloff@cs.ucdavis.edu>
2015-08-25 23:10:20 -0700

    arg change etc.

--- R/Nonpar.R
+++ R/Nonpar.R
@@ -1,45 +1,57 @@
 
 ###########################  k-NN  ###############################
 
+# k-NN estimation and prediction
+
+# estimation:`
+
 # arguments:
 #
-#  xydata: matrix or data frame of (X,Y) data, 
-#          Y in last column
-#  predpts:  matrix or data frame of X values at
-#              which to estimate the regression ftn
+#   xydata: matrix or data frame of (X,Y) data, 
+#           Y in last column
 #   k:  number of nearest neighbors
-#   scalefirst: call scale() on the data first
+#   scalefirst: call scale() on the data first; NULL means no scaling,
+#               'default' means call scale() with defaults, and
+#               a length-2 numeric vector means call scale() with these
+#               center and scale values
 #   nearf: function to apply to the nearest neighbors 
 #          of a point
 #
-# value: estimated reg. ftn. at the given X values predpts
+# value: cbind() of X values in xydata and estimated reg. ftn. at those
+#        values, contains 'center' and 'scale' attributes if scalefirst
+#        is non-NULL
 
-knnest <- function(xydata,predpts=xydata[,-ncol(xydata)],
-   k,scalefirst=FALSE,nearf=meany)
+knnest <- function(xydata,k,predpts=xydata[,-ncol(xydata),drop=FALSE],
+   scalefirst=NULL,nearf=meany,outfile=NULL)
 {  require(FNN)
-   if (is.null(predpts))
-      predpts <- xydata[,-ncol(xydata),drop=FALSE]
-   if (is.vector(predpts))
-      predpts <- matrix(predpts,ncol=1)
    ycol <- ncol(xydata)  # where is Y?
    # extract the X and Y data
    x <- xydata[,-ycol,drop = F]
    y <- xydata[,ycol]
-   colnames(predpts) <- colnames(x)
-   if (scalefirst) {
-      tmp <- rbind(x,predpts)
-      tmp <- scale(tmp)
-      x <- tmp[1:nrow(x),,drop=FALSE]
-      predpts <- tmp[(nrow(x)+1):nrow(tmp),,drop=FALSE]
+   sf <- scalefirst
+   if (!is.null(sf)) {
+      if (is.character(sf)) {
+         x <- scale(x) 
+      } else
+         x <- scale(x,center=sf[1],scale=sf[2])
+      xydata <- cbind(x,y)
    }
-   tmp <- get.knnx(data=x, query=predpts, k=k)
+   tmp <- get.knnx(data=x, query=x, k=k)
    idx <- tmp$nn.index
    # set idxrows[[i]] to row i of idx
    idxrows <- matrixtolist(1,idx)
    nearxy <- lapply(idxrows,function(idxrow) xydata[idxrow,])
-   sapply(1:nrow(predpts),
-      function(i) nearf(predpts[i,],nearxy[[i]]))
+   regest <- sapply(1:nrow(x),
+      function(i) nearf(x[i,],nearxy[[i]]))
+   knnout <- cbind(x,regest)
+   attr(knnout,'center') <- attr(x,'scaled:center')
+   attr(knnout,'scale') <- attr(x,'scaled:scale')
+   knnout
 }
+
+# prediction
+
+# knnpred
 
 # find mean of Y on the data z, Y in last column, and predict at xnew
 meany <- function(predpt,nearxy) {
@@ -67,4 +79,16 @@ matrixtolist <- function (rc, m)
     }
     else Map(function(colnum) m[, colnum], 1:ncol(m))
 }
+
+# sgm <- 1
+# a <- 5
+# b <- 8
+# ones <- matrix(rep(1,p),ncol=1)
+# z <- matrix(nrow = n, ncol = p1)
+# z[,1:p] <- runif(n*p,min=a,max=b)
+# z[,p1] <- z[,1:p] %*% ones + sgm * runif(n,min = -0.5,max = 0.5)
+# zout <- knnest(z,50,scalefirst='default')
+# plot(z[,3],zout[,3]) # incorrect flattening at the 2 ends
+# zout <- knnest(z,50,scalefirst='default',nearf=loclin)
+# plot(z[,3],zout[,3]) # 
 

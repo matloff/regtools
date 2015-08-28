2986fd309a8a2ef00010a9a1dde1cd2de540ce12
Norm Matloff <matloff@cs.ucdavis.edu>
2015-08-27 22:18:14 -0700

    added nlshc()

---
+++ R/Nonlin.R
@@ -0,0 +1,37 @@
+
+# nonlinear regression like nls(), but without 
+# assuming homoscedasticity
+
+# uses output of nlsLM() of the minpack.lm package 
+# to get an asymptotic covariance matrix in general
+# heteroscedastic case
+
+# arguments:
+# 
+#    nlslmout: return value from nlsLM()
+# 
+# value: approximate covariance matrix for the 
+#        estimated parameter vector
+
+nlshc <- function(nlslmout) {
+   require(minpack.lm)
+   require(sandwich)
+   # notation: g(t,b) is the regression model, 
+   # where t is the vector of variables for a 
+   # given observation; b is the estimated parameter
+   # vector; x is the matrix of predictor values
+   b <- coef(nlslmout)
+   m <- nlslmout$m
+   # y - g:
+   resid <- m$resid()
+   # row i of hmat will be deriv of g(x[i,],b) 
+   # with respect to b
+   hmat <- m$gradient()
+   # calculate the artificial "x" and "y" of 
+   # the algorithm
+   xhm <- hmat
+   yresidhm <- resid + hmat %*% b
+   # -1 means no constant term in the model
+   lmout <- lm(yresidhm ~ xhm - 1)
+   vcovHC(lmout)
+}

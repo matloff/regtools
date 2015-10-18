5374bef11d7a9cef4e8b246c5cb12425b0b5ade7
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2015-10-18 15:06:30 -0700

    more devel

--- R/MultLogit.R
+++ R/MultLogit.R
@@ -9,30 +9,53 @@
 #    m:  number of classes
 #    trnxy:  X, Y training set; Y in last column; Y coded 0,1,...,m-1
 #            for the m classes
-
+#    predx:  X values from which to predict Y values
 #    tstxy:  X, Y test set, same format
 
-# generate estimated regression functions
+##################################################################
+# ovalogtrn: generate estimated regression functions
+##################################################################
 
 # arguments:
 
-#    as above
+#    m:  as above
+#    trnxy:  as above
 
 # value:
 
-#    matrix of the betahat vectors, one per row
-#    cc (correct classification propoirton) for the test set
+#    matrix of the betahat vectors, one per column
  
-ovalog <- function(trnxy,tstxy() {
+ovalogtrn <- function(m,trnxy) {
    p <- ncol(trnxy) 
    x <- trnxy[1:(p-1)]
    y <- trnxy[,p]
    outmat <- NULL
    y <- 
-   for (i in01:(m-1)) {
+   for (i in 0:(m-1)) {
       ym <- as.integer(y == i)
       betahat <- coef(glm(ym ~ x,family=binomial))
+      outmat <- rbind(outmat,betahat)
    }
+   outmat
+}
 
+##################################################################
+# ovalogpred: predict sY from new Xs
+##################################################################
+
+# arguments:  
+# 
+#    coefmat:  coefficient matrix, output from ovalogtrn()
+#    predx:  as above
+# 
+# value:
+# 
+#    vector of predicted Y values, in {0,1,...,m-1}
+
+ovalogpred <- function(coefmat,predx) {
+   tmp <- cbind(1,predx) %*% coefmat
+   # since the logit function is monotone increeasing and we are just
+   # taking argmax, we need not plug into logit
+   predy <- apply(tmp,1,which.max)
 }
 

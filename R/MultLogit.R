90bc0f7ac162c1f078ccb9097885f662f6a9077c
Norman Matloff <normanmatloff@unknown80e65050fa73.att.net>
2015-10-28 16:13:58 -0700

    progress

--- R/MultLogit.R
+++ R/MultLogit.R
@@ -27,7 +27,7 @@
 
 ovalogtrn <- function(m,trnxy) {
    p <- ncol(trnxy) 
-   x <- as.matrix(trnxy[1:(p-1)])
+   x <- as.matrix(trnxy[,1:(p-1)])
    y <- trnxy[,p]
    outmat <- NULL
    for (i in 0:(m-1)) {
@@ -51,36 +51,83 @@ ovalogtrn <- function(m,trnxy) {
 # 
 #    vector of predicted Y values, in {0,1,...,m-1}
 
-ovalogpred <- function(coefmat,predxy) {
+ovalogpred <- function(coefmat,predx) {
    # get est reg ftn values for each row of predx and each col of
    # coefmat; vals from coefmat[,] in tmp[,i]
-   tmp <- cbind(1,predx) %*% coefmat
+   tmp <- as.matrix(cbind(1,predx)) %*% coefmat
    tmp <- logit(tmp)
    apply(tmp,1,which.max) - 1
 }
 
+##################################################################
+# avalogtrn: generate estimated regression functions
+##################################################################
+
+# arguments:
+
+#    m:  as above
+#    trnxy:  as above
+
+# value:
+
+#    matrix of the betahat vectors, one per column, in the order of
+#    combin()
+
+avalogtrn <- function(m,trnxy) {
+   p <- ncol(trnxy) 
+   x <- as.matrix(trnxy[,1:(p-1)])
+   y <- trnxy[,p]
+   outmat <- NULL
+   ijs <- combn(p-1,2)
+   doreg <- function(ij) {
+      i <- ij[1]
+      ym <- as.integer(y == i)
+      coef(glm(ym ~ x[,ij],family=binomial))
+   }
+   sapply(ijs,doreg)
+}
+
+##################################################################
+# avalogpred: predict Ys from new Xs
+##################################################################
+
+# arguments:  
+# 
+#    coefmat:  coefficient matrix, output from avalogtrn()
+#    predx:  as above
+# 
+# value:
+# 
+#    vector of predicted Y values, in {0,1,...,m-1}
+
+avalogpred <- function(coefmat,predx) {
+   ijs <- combn(p-1,2)
+   # get est reg ftn values for each row of predx and each col of
+   # coefmat; vals from coefmat[,] in tmp[,i]
+
+
+}
+
 logit <- function(t) 1 / (1+exp(-t))
 
+# ucbdf <- tbltofakedf(UCBAdmissions)
+# newucb <- matrix(nrow=nrow(ucbdf),ncol=ncol(ucbdf))
+# for (i in 1:3) {
+#    z <- ucbdf[,i] 
+#    z <- as.numeric(as.factor(z))
+#    newucb[,i] <- z
+# }
+# newucb[,3] <- newucb[,3] - 1
+# ovalogtrn(6,newucb)
 
-ucbdf <- tbltofakedf(UCBAdmissions)
-z <- ucbdf[,1] 
-ucbdf[,1] <- ifelse(z=="Admitted",1,0)
-z <- ucbdf[,2] 
-ucbdf[,2] <- ifelse(z=="Male",1,0)
-z <- ucbdf[,3] 
-f <- function(chr) charToRaw(chr)
-ucbdf[,3] <- asc(z) - 65
-ucbdf <- as.data.frame(ucbdf)
-for (i in 1:3) ucbdf[,i] <- as.numeric(ucbdf[,i])
-ovalogtrn(6,ucbdf)
-
-forest <- read.csv("~/Research/Data/ForestTypes/training.csv")
-z <- forest[,1]
-z <- as.numeric(z)
-z <- z - 1
-forest[,1] <- z
-f1 <- cbind(forest[,-1],forest[,1]) 
-f2 <- f1[,-(1:20)]
-ovalogtrn(4,f2)
-
+# forest <- read.csv("~/Research/Data/ForestTypes/training.csv")
+# z <- forest[,1]
+# z <- as.numeric(z)
+# z <- z - 1
+# forest[,1] <- z
+# f1 <- cbind(forest[,-1],forest[,1]) 
+# f2 <- f1[,-(1:20)]
+# ovalogtrn(4,f2)
+ypred <- ovalogpred(ovout,newucb[,1:2])
+sum(ypred == newucb[,3])
 

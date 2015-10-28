cda3d3d5867ac07c76b4224738756474947ae908
Norm Matloff <matloff@cs.ucdavis.edu>
2015-10-27 23:01:07 -0700

    progress

--- R/MultLogit.R
+++ R/MultLogit.R
@@ -24,10 +24,10 @@
 # value:
 
 #    matrix of the betahat vectors, one per column
- 
+
 ovalogtrn <- function(m,trnxy) {
    p <- ncol(trnxy) 
-   x <- trnxy[1:(p-1)]
+   x <- as.matrix(trnxy[1:(p-1)])
    y <- trnxy[,p]
    outmat <- NULL
    for (i in 0:(m-1)) {
@@ -39,24 +39,48 @@ ovalogtrn <- function(m,trnxy) {
 }
 
 ##################################################################
-# ovalogpred: predict sY from new Xs
+# ovalogpred: predict Ys from new Xs
 ##################################################################
 
 # arguments:  
 # 
 #    coefmat:  coefficient matrix, output from ovalogtrn()
-#    predxy:  X,Y prediction set, Y in last column
+#    predx:  as above
 # 
 # value:
 # 
 #    vector of predicted Y values, in {0,1,...,m-1}
 
 ovalogpred <- function(coefmat,predxy) {
-   predx <- predxy[,-1]
    # get est reg ftn values for each row of predx and each col of
-   # coefmat; vals for coefmat[,] in col i of tmp
+   # coefmat; vals from coefmat[,] in tmp[,i]
    tmp <- cbind(1,predx) %*% coefmat
    tmp <- logit(tmp)
-   predy <- apply(estlogodds,1,which.max)
+   apply(tmp,1,which.max) - 1
 }
+
+logit <- function(t) 1 / (1+exp(-t))
+
+
+ucbdf <- tbltofakedf(UCBAdmissions)
+z <- ucbdf[,1] 
+ucbdf[,1] <- ifelse(z=="Admitted",1,0)
+z <- ucbdf[,2] 
+ucbdf[,2] <- ifelse(z=="Male",1,0)
+z <- ucbdf[,3] 
+f <- function(chr) charToRaw(chr)
+ucbdf[,3] <- asc(z) - 65
+ucbdf <- as.data.frame(ucbdf)
+for (i in 1:3) ucbdf[,i] <- as.numeric(ucbdf[,i])
+ovalogtrn(6,ucbdf)
+
+forest <- read.csv("~/Research/Data/ForestTypes/training.csv")
+z <- forest[,1]
+z <- as.numeric(z)
+z <- z - 1
+forest[,1] <- z
+f1 <- cbind(forest[,-1],forest[,1]) 
+f2 <- f1[,-(1:20)]
+ovalogtrn(4,f2)
+
 

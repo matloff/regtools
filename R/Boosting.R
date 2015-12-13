54ce2803a28ec328b9646a50da236d4df39716f8
Norm Matloff <matloff@cs.ucdavis.edu>
2015-12-12 23:16:54 -0800

    Berk boosting!

---
+++ R/Boosting.R
@@ -0,0 +1,64 @@
+
+# "BerkBoost" -- inspired by Berk, p.21
+
+# arguments:
+# 
+#    x: predictor matrix
+#    y: response vector
+#    wtftn: function to weight each observation
+#    lossftn: loss function for observation
+#    niters: number of iterations
+#    maxwt: lm() weights will be truncated to this
+# 
+# value:
+# 
+#    boosted beta-hat vector
+
+bboostlm <- function(x,y,wtftn=l2inv,lossftn=l1,niters=1000,maxwt=100) {
+   x <- as.matrix(x)
+   tmp <- lm(y ~ x)
+   fit <- tmp$fitted.values
+   sumloss <- sum(lossftn(y,fit))
+   bhmat <- c(coef(tmp),sumloss)
+   for (i in 1:niters) {
+      w <- wtftn(y,fit)
+      w <- pmin(w,maxwt)
+      tmp <- lm(y ~ x, weights=w)
+      fit <- tmp$fitted.values
+      sumloss <- sum(lossftn(y,fit))
+      newbhmat <- c(coef(tmp),sumloss)
+      bhmat <- rbind(bhmat,newbhmat)
+   }
+   p <- ncol(bhmat) - 1
+   p1 <- p + 1
+   w <- 1 / bhmat[,p1]
+   w <- pmin(w,1.0e+04)
+   w <- w / sum(w)
+   rslt <- list()
+   rslt$bhat = w %*% bhmat[,1:p] 
+   class(rslt) <- "bboost"
+   rslt
+}
+
+# pred() for "bboost" class
+#
+# arguments:
+#    bboost: output of bboostlm()
+#    predx: X vectors to predict at, one row per case
+#
+# value:
+#    predicted Y values
+predict.bboost <- function(bbout,predx) {
+   bhat <- as.vector(bbout$bhat)
+   predx1 <- as.matrix(cbind(1,predx))
+   predx1 %*% bhat
+}
+
+l2 <- function(y,muhat) (y - muhat)^2
+
+l1 <- function(y,muhat) abs(y - muhat)
+
+l2inv <- function(y,muhat) 1 / (y - muhat)^2
+
+l1inv <- function(y,muhat) 1 / abs(y - muhat)
+

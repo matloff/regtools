a67fa545174ccf7ebdb730eaf5b16390712a4724
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2016-02-03 15:14:41 -0800

    new version of mm()

--- R/MM.R
+++ R/MM.R
@@ -1,62 +1,64 @@
 
-# Augmented Method of Moments
+# Method of Moments, including regression terms
 
 # overview:
 
 #    motivated by linear mixed effects models, but more general
 # 
-#    say the parameter vector theta has length k, so we need k equations
-# 
-#    r of the equations will be classical Method of Moments, and k-r will
-#    be regression type, i.e. the (X'X)^{-1} betahat = X'Y type; these
-#    could also be linear approximations, as in glm()
+#    say the parameter vector theta has length k, so we need k
+#    equations; their left-hand sides are specified by the argument g,
+#    while the right-hand sides are given by mm; the user integrates
+#    regression terms into these two function arguments
 
 # arguments:
 # 
-#    mm(x): vector of sample moments ("left-hand sides" of MM eqns); 
-#           x is the data, one observation per row; might be more
-#           general than moments
+#   mm: a vector of sample moments ("left-hand sides" of MM eqns); 
+#       x is the data, one observation per row; might be more
+#       general than moments
 #    g(x,theta): 
 #       a vector-valued function, specifying the "right-hand sides" 
 #       of the MM eqns; x as above, and theta is the vector of 
 #       parameters to be estimated; it is required that the second 
 #       argument of g() be named 'theta'
-#    data: our x in m() and g()
-#    lg: length(m), i.e. number of equations
+#    x: our x in m and g()
+#    lth: length of theta
 #    init: initial guess for theta; R list with names corresponding
 #          to the parameters in g
 #    eps: convergence criterion; iterations stop at 1000, or whe
 #         sum(abs(g)) < eps
 #    maxiters: max number of iterations
 
-amm <- function(m,g,x,lg,init=rep(0.5,lg),eps=0.0001,maxiters=1000) {
-   tht <- unlist(init)
-   mvec <- m(data)
+mm <- function(m,g,x,lth=length(m),
+      init=rep(0.5,lth),eps=0.0001,maxiters=1000) {
+   tht <- init
+   # mvec <- m(data)
+   mvec <- m
    for (i in 1:maxiters) {
-      # current g values
-      gvec <- getgvec(g,tht)
+      # g values for current iteration
+      # gvec <- getgvec(g,tht)
+      # browser()
+      gvec <- g(x,tht)
       if (max(abs(mvec - gvec)) < eps) {
          if (!is.null(names(init))) 
             names(tht) <- names(init)
-
          result <- list(tht=tht,numiters=i)
          return(tht)
       }
-      # Jacobian
-      jcb <- getjcb(g,tht)
+      # not done, so get new Jacobian and update tht
+      jcb <- getjcb(g,x,tht)
       tht <- tht + solve(jcb,mvec-gvec)
    }
    print('max iterations exceeded')
 }
 
-getgvec <- function(g,tht) {
-   theta <- tht
-   g(theta)
-}
+# getgvec <- function(g,tht) {
+#    theta <- tht
+#    g(theta)
+# }
 
-getjcb <- function(g,tht) {
+getjcb <- function(g,x,tht) {
    theta <- tht
-   attr(numericDeriv(quote(g(theta)),'theta'),'gradient')
+   attr(numericDeriv(quote(g(x,theta)),'theta'),'gradient')
 }
 
 # test case; should output about 2 and 1

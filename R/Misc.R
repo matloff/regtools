1607cd6001924c3fd3f7e122458f36a00fd0bedc
Norman Matloff <matloff@cs.ucdavis.edu>
2019-06-30 16:02:52 -0700

    more prgeng housecleaning

--- R/Misc.R
+++ R/Misc.R
@@ -18,37 +18,39 @@ unscale <- function(scaledx,ctrs=NULL,sds=NULL) {
    origx
 }
 
-# convenience function to load and prep the 'prgeng' data; creates 
-# global data frame pe; if Dummies, then factors are changed to 
-# dummy variables
-getPE <- function(Dummies=TRUE)
-{
-   data(prgeng)
-   pe <- prgeng
-   pe$sex <- 2 - pe$sex
-   if (!Dummies) {
-      pe <<- pe
-      return()
-   }
-   # dummies for MS, PhD etc.
-   pe$ms <- as.integer(pe$educ == 14)
-   pe$phd <- as.integer(pe$educ == 16)
-   pe$educ <- NULL
-   pe$engl <- NULL
-   pe$birth <- NULL
-   pe$powspuma <- NULL
-   pe$yrentry <- ifelse(pe$yrentry == 0,round(2000-pe$age),pe$yrentry)
-   require(dummies)
-   citstatus <- dummy(pe$cit)[,1:4]
-   colnames(citstatus) <- c('cit1','cit2','cit3','cit4')
-   pe <- cbind(pe,citstatus)
-   pe$cit <- NULL
-   occcode <- dummy(pe$occ)[,1:5]
-   colnames(occcode) <- c('occ1','occ2','occ3','occ4','occ5')
-   pe <- cbind(pe,occcode)
-   pe$occ <- NULL
-   pe <<- pe
-}
+# not needed anymore, since various versions of the dataset are now
+# available directly via data(); see ?prgeng
+% # convenience function to load and prep the 'prgeng' data; creates 
+% # global data frame pe; if Dummies, then factors are changed to 
+% # dummy variables
+% getPE <- function(Dummies=TRUE)
+% {
+%    data(prgeng)
+%    pe <- prgeng
+%    pe$sex <- 2 - pe$sex
+%    if (!Dummies) {
+%       pe <<- pe
+%       return()
+%    }
+%    # dummies for MS, PhD etc.
+%    pe$ms <- as.integer(pe$educ == 14)
+%    pe$phd <- as.integer(pe$educ == 16)
+%    pe$educ <- NULL
+%    pe$engl <- NULL
+%    pe$birth <- NULL
+%    pe$powspuma <- NULL
+%    pe$yrentry <- ifelse(pe$yrentry == 0,round(2000-pe$age),pe$yrentry)
+%    require(dummies)
+%    citstatus <- dummy(pe$cit)[,1:4]
+%    colnames(citstatus) <- c('cit1','cit2','cit3','cit4')
+%    pe <- cbind(pe,citstatus)
+%    pe$cit <- NULL
+%    occcode <- dummy(pe$occ)[,1:5]
+%    colnames(occcode) <- c('occ1','occ2','occ3','occ4','occ5')
+%    pe <- cbind(pe,occcode)
+%    pe$occ <- NULL
+%    pe <<- pe
+% }
 
 # make a matrix or data frame (fmt = ''' or 'd') of 
 # dummies from a factor f; col names will be 'dms' concatenated

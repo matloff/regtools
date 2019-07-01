653dc706258c4a8b02c1f140860852db7c9e549c
Norman Matloff <matloff@cs.ucdavis.edu>
2019-07-01 14:05:53 -0700

    cleaning/expanding dumms/factors conversion

--- R/Misc.R
+++ R/Misc.R
@@ -1,4 +1,6 @@
 
+#########################  unscale()  #################################
+
 # undoes 'scale()'
 
 # reverses scaling on scaledx, dividing its columns by sds and adding
@@ -18,73 +20,45 @@ unscale <- function(scaledx,ctrs=NULL,sds=NULL) {
    origx
 }
 
-# not needed anymore, since various versions of the dataset are now
-# available directly via data(); see ?prgeng
-# # convenience function to load and prep the 'prgeng' data; creates 
-# # global data frame pe; if Dummies, then factors are changed to 
-# # dummy variables
-# getPE <- function(Dummies=TRUE)
+#################  convert between factors and dummies  ##################
+
+# these routines are useful in that some regression packages insist that
+# predictor be factors, while some require dummy variables
+
+# make a matrix or data frame (fmt = 'm' or 'd') of # dummies from a
+# factor f; column names will be 'dms' concatenated with the factor
+# levels
+
+# factorToDummies <- function(f,fmt='m')
 # {
-#    data(prgeng)
-#    pe <- prgeng
-#    pe$sex <- 2 - pe$sex
-#    if (!Dummies) {
-#       pe <<- pe
-#       return()
+#    n <- length(f)
+#    fl <- levels(f)
+#    ndumms <- length(fl) - 1
+#    dms <- if(fmt=='m') {
+#       matrix(nrow=n,ncol=ndumms) 
 #    }
-#    # dummies for MS, PhD etc.
-#    pe$ms <- as.integer(pe$educ == 14)
-#    pe$phd <- as.integer(pe$educ == 16)
-#    pe$educ <- NULL
-#    pe$engl <- NULL
-#    pe$birth <- NULL
-#    pe$powspuma <- NULL
-#    pe$yrentry <- ifelse(pe$yrentry == 0,round(2000-pe$age),pe$yrentry)
-#    require(dummies)
-#    citstatus <- dummy(pe$cit)[,1:4]
-#    colnames(citstatus) <- c('cit1','cit2','cit3','cit4')
-#    pe <- cbind(pe,citstatus)
-#    pe$cit <- NULL
-#    occcode <- dummy(pe$occ)[,1:5]
-#    colnames(occcode) <- c('occ1','occ2','occ3','occ4','occ5')
-#    pe <- cbind(pe,occcode)
-#    pe$occ <- NULL
-#    pe <<- pe
+#    else 
+#       data.frame(nrow=n,ncol=ndumms)
+#    for (i in 1:ndumms) 
+#       dms[,i] <- as.integer(f == fl[i])
+#    colnames(dms) <- paste('dms',fl[-(length(fl))],sep='')
+#    dms
 # }
 
-# make a matrix or data frame (fmt = ''' or 'd') of 
-# dummies from a factor f; col names will be 'dms' concatenated
-# with the factor levels
-factorToDummies <- function(f,fmt='m')
-{
-   n <- length(f)
-   fl <- levels(f)
-   ndumms <- length(fl) - 1
-   dms <- if(fmt=='m') {
-      matrix(nrow=n,ncol=ndumms) 
-   }
-   else 
-      data.frame(nrow=n,ncol=ndumms)
-   for (i in 1:ndumms) 
-      dms[,i] <- as.integer(f == fl[i])
-   colnames(dms) <- paste('dms',fl[-(length(fl))],sep='')
-   dms
-}
-
-# for each column in df, if factor then replace by dummies, all but last
+# for each column in dfr, if factor then replace by dummies, all but last
 # class, else just copy column
 
-factorsToDummies <- function(df) 
+factorsToDummies <- function(dfr) 
 {
    require(dummies)
-   outDF <- data.frame(rep(0,nrow((df))))  # filler start
-   for (i in 1:ncol(df)) {
-      dfi <- df[,i]
+   outDF <- data.frame(rep(0,nrow((dfr))))  # filler start
+   for (i in 1:ncol(dfr)) {
+      dfi <- dfr[,i]
       if (!is.factor(dfi)) {
          outDF <- cbind(outDF,dfi) 
-         names(outDF)[ncol(outDF)] <- names(df)[i]
+         names(outDF)[ncol(outDF)] <- names(dfr)[i]
       } else {
-         dumms <- factorToDummies(dfi,names(df)[i])
+         dumms <- factorToDummies(dfi,names(dfr)[i])
          outDF <- cbind(outDF,dumms)
       }
    }
@@ -124,3 +98,38 @@ checkConstCol <- function(col)
    } else return(FALSE)
 }
 
+#########################  REMOVED  #################################
+
+# not needed anymore, since various versions of the dataset are now
+# available directly via data(); see ?prgeng
+# # convenience function to load and prep the 'prgeng' data; creates 
+# # global data frame pe; if Dummies, then factors are changed to 
+# # dummy variables
+# getPE <- function(Dummies=TRUE)
+# {
+#    data(prgeng)
+#    pe <- prgeng
+#    pe$sex <- 2 - pe$sex
+#    if (!Dummies) {
+#       pe <<- pe
+#       return()
+#    }
+#    # dummies for MS, PhD etc.
+#    pe$ms <- as.integer(pe$educ == 14)
+#    pe$phd <- as.integer(pe$educ == 16)
+#    pe$educ <- NULL
+#    pe$engl <- NULL
+#    pe$birth <- NULL
+#    pe$powspuma <- NULL
+#    pe$yrentry <- ifelse(pe$yrentry == 0,round(2000-pe$age),pe$yrentry)
+#    require(dummies)
+#    citstatus <- dummy(pe$cit)[,1:4]
+#    colnames(citstatus) <- c('cit1','cit2','cit3','cit4')
+#    pe <- cbind(pe,citstatus)
+#    pe$cit <- NULL
+#    occcode <- dummy(pe$occ)[,1:5]
+#    colnames(occcode) <- c('occ1','occ2','occ3','occ4','occ5')
+#    pe <- cbind(pe,occcode)
+#    pe$occ <- NULL
+#    pe <<- pe
+# }

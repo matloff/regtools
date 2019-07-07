a86922a42966cfd90da29e87bbec406f89bbd079
Norman Matloff <matloff@cs.ucdavis.edu>
2019-07-07 16:05:58 -0700

    logon msg

---
+++ R/onAttach.R
@@ -0,0 +1,6 @@
+
+.onAttach <- function(libname, pkgname) {
+   packageStartupMessage(
+      '\n\n\n\n\n*********************\n\n\n\nLatest version of regtools at GitHub.com/matloff\n\n\nType "?regtools" for function list.')
+}
+

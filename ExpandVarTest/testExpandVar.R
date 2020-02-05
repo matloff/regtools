42b9434f6853e23e25b349a2c080a010632825a3
ericXin95616 <xinbochao97@outlook.com>
2020-02-04 23:46:37 -0800

    more expand test on forestCoverType dataset

--- ExpandVarTest/testExpandVar.R
+++ ExpandVarTest/testExpandVar.R
@@ -35,61 +35,32 @@ testSingleExpandVar <- function(x, y, kmax, expandCol, expandMaxVal, interval = 
     theme(plot.title = element_text(hjust = 0.5))
 }
 
-
-
-# this function is used to test how different combinations of expanding
-# variables will affect our prediction results
-# it will output a scatter point graph
-
-# x, y, all are all the same as above
-# but now expandCols is an vector containing all cols we want to expand
-# expandVals containing all the corresponding expanding values
-
-# this function should not be used when expandCols is bigger than 10
-# because it will take too long for function to finish
-testMultipleExpandVar <- function(x, y, kmax, expandCols, expandVals, all = TRUE)
+# class version of testSingleExpandVar
+testSingleExpandVar_class <- function(x, y, kmax, newx, newy, expandCol, expandMaxVal, interval = 1, all = FALSE)
 {
   require(ggplot2)
-  numExpandVar <- length(expandCols)
-  
-  # base case -- no expansion
-  knnout <- basicKNN(x,y,x,kmax, scaleX = TRUE, allK = all, leave1out = TRUE)
-  
-  if(!is.matrix(knnout$regests)) {
-    dim(knnout$regests) <- c(1,length(knnout$regests))
-  }
-  
-  mape <- findOverallLoss(knnout$regests, y)
-  d <- data.frame(0, min(mape))
-  names(d) <- c("Number_Of_Expansion", "MAPE")
-  
-  # expand case
-  for (i in 1:numExpandVar) {
-    combCases <- combn(expandCols, i)
-    numCases <- ncol(combCases)
-    for (j in 1:numCases) {
-      # get their position
-      # it works fine because expandCols should not have repeated values
-      logicalVec <- expandCols %in% combCases[, j]
-      knnout <- kNN(x,y,x,kmax, scaleX = TRUE, expandVars = combCases[, j],
-                         expandVals = expandVals[logicalVec], allK = all, leave1out = TRUE)
-
-      if(!is.matrix(knnout$regests)) {
-        dim(knnout$regests) <- c(1,length(knnout$regests))
-      }
-      
-      mape <- findOverallLoss(knnout$regests, y)
-      
-      tmp <-data.frame(i, min(mape))
-      names(tmp) <- c("Number_Of_Expansion", "MAPE")
-      d <- rbind(d, tmp)
+  res <- NULL
+  allExpandVals <- seq(0, expandMaxVal, interval)
+  for(i in allExpandVals){
+    knnout <- kNN(x,y,newx,kmax,scaleX = TRUE, expandVars = expandCol, expandVals = i, allK = all, leave1out = TRUE, classif = TRUE)
+    # when @all == FALSE, regests is not matrix and cannot be used in findOverallLoss
+    if(!is.matrix(knnout$ypreds)) {
+      dim(knnout$ypreds) <- c(1,length(knnout$ypreds))
     }
+    probCorrect <- findOverallLoss(knnout$ypreds, newy, lossFtn = probCorrectClass)
+    # print(probCorrect)
+    res <- c(res, min(probCorrect))
   }
-  # plotting scatter point graphs
-  ggplot(d, aes(x=Number_Of_Expansion, y=MAPE)) + geom_point()
+  
+  dat <- as.data.frame(cbind(allExpandVals, res))
+  total_pts <- length(allExpandVals)
+  # using 3 percent of points to do local regression
+  span_pts <- 0.03*total_pts
+  ggplot(dat, aes(x=allExpandVals,y=res)) + geom_smooth(method=loess, span=span_pts, colour = "black") +
+    labs(title = "Percentage Correct vs. Expanded Variable's Value") +  ylab("Percentage Correct") + 
+    xlab("Expanded Variable's Value") +
+    theme(plot.title = element_text(hjust = 0.5))
 }
-
-
 
 # First test dataset: day1
 # predict @tot based on the following 11 predictors
@@ -204,30 +175,6 @@ if(FALSE) {
   testMultipleExpandVar(x,y,60,1:7,rep(2,7), FALSE)
 }
 
-# Fourth test dataset:MNIST of handwritten digits
-# This is tested how knn works for dataset with larget p
-require(keras)
-require(tensorflow)
-
-MNIST <- dataset_mnist(path = "mnist.npz")
-# the origin dataset might take very long time to finish
-# we only use test set of MNIST and predict test set itself
-x <- MNIST$test$x
-y <- MNIST$test$y
-
-# x is a 3d matrix, we want to convert it to 2d matrix
-x <- matrix(x, nrow = 10000, ncol = 28*28)
-# convert y from an array to an vector
-y <- factor(c(as.matrix(y)))
-# convert y to dummy variables
-# we don't need to omit last col because y is not predictor
-y <- factorToDummies(y, "digit", omitLast = FALSE)
-
-if(FALSE) {
-  # expand single predictor
-  # expand point at the center
-  testSingleExpandVar(x,y,)
-}
 
 # Fifth test dataset: forest cover dataset
 # Download dataset
@@ -253,8 +200,35 @@ if(FALSE){
 # Soil_Type (40 binary columns)           qualitative     0 (absence) or 1 (presence)  Soil Type designation
 # Cover_Type (7 types)                    integer         1 to 7                       Forest Cover Type designation
 forestCoverTypeX <- forestCoverType[,-55]
-forestCoverTypeY <- forestCoverType[,55]
-# using k =
+forestCoverTypeY <- forestCoverType[,55, drop=FALSE]
+set.seed(99)
+# select 10,000 samples, otherwise it takes forever to finish
+selectRows <- sample(1:nrow(forestCoverType), 10000, replace = FALSE)
+forestCoverTypeNewX <- forestCoverType[selectRows,-55]
+forestCoverTypeNewY <- forestCoverType[selectRows,55, drop=FALSE]
+
+# knnout <- kNN(forestCoverTypeX, forestCoverTypeY, forestCoverTypeNewX, k = 100, allK = TRUE, leave1out = TRUE, classif = TRUE)
+# findOverallLoss(knnout$ypreds, forestCoverTypeNewY, lossFtn = probCorrectClass)
+# result:
+# [1] 0.9407 0.9223 0.9141 0.9013 0.8923 0.8898 0.8928 0.8868 0.8801 0.8717 0.8680 0.8661 0.8733 0.8664 0.8636 0.8579 0.8543
+# [18] 0.8512 0.8514 0.8458 0.8427 0.8393 0.8378 0.8363 0.8389 0.8349 0.8313 0.8289 0.8261 0.8251 0.8255 0.8224 0.8206 0.8179
+# [35] 0.8167 0.8149 0.8170 0.8138 0.8121 0.8089 0.8083 0.8079 0.8066 0.8043 0.8029 0.8005 0.8007 0.7980 0.8005 0.7981 0.7986
+# [52] 0.7964 0.7962 0.7941 0.7935 0.7914 0.7898 0.7885 0.7877 0.7862 0.7876 0.7871 0.7858 0.7841 0.7817 0.7810 0.7801 0.7781
+# [69] 0.7772 0.7753 0.7748 0.7739 0.7750 0.7736 0.7724 0.7710 0.7703 0.7701 0.7689 0.7684 0.7669 0.7666 0.7656 0.7657 0.7667
+# [86] 0.7666 0.7654 0.7651 0.7640 0.7627 0.7640 0.7627 0.7618 0.7612 0.7605 0.7597 0.7619 0.7608 0.7599 0.7593
+
+# using k = 10
 if(FALSE){
+  # Expand elevation
+  testSingleExpandVar_class(forestCoverTypeX, forestCoverTypeY, 10, forestCoverTypeNewX, forestCoverTypeNewY, 1, 10, 0.1)
+  testSingleExpandVar_class(forestCoverTypeX, forestCoverTypeY, 10, forestCoverTypeNewX, forestCoverTypeNewY, 1, 10, 0.01)
+  testSingleExpandVar_class(forestCoverTypeX, forestCoverTypeY, 10, forestCoverTypeNewX, forestCoverTypeNewY, 1, 100, 0.1)
   
+  # Expand Aspect
+  testSingleExpandVar_class(forestCoverTypeX, forestCoverTypeY, 10, forestCoverTypeNewX, forestCoverTypeNewY, 2, 10, 0.01)
+  testSingleExpandVar_class(forestCoverTypeX, forestCoverTypeY, 10, forestCoverTypeNewX, forestCoverTypeNewY, 2, 1000, 0.5)
+  
+  # Expand Slope
+  testSingleExpandVar_class(forestCoverTypeX, forestCoverTypeY, 10, forestCoverTypeNewX, forestCoverTypeNewY, 3, 10, 0.01)
+  testSingleExpandVar_class(forestCoverTypeX, forestCoverTypeY, 10, forestCoverTypeNewX, forestCoverTypeNewY, 3, 1000, 0.5)
 }

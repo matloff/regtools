9d200723bb2630255a7e7a723e86b7410acd4f1a
matloff <matloff@cs.ucdavis.edu>
2020-02-19 23:13:33 -0800

    got past the hard part

--- R/FineTuning.R
+++ R/FineTuning.R
@@ -4,13 +4,16 @@
 
 # arguments:
 
+#   dataset: data frame or equivalent, containing the "X" and "Y"
+#      columns; will be split by the function to 'dtrn' and 'dtst',
+#      same level as the call
 #   pars: named R list, one component for each parameter; component 'x'
 #      is the set of desired values for the parameter 'x'
-#   theCall: character string giving the call for given regression/
-#      classification method, together with the associated prediction
-#      function and loss evaluation; the elements of names(pars) 
-#      will appear; the data is assumed to be either global or at the
-#      same level is the call
+#   theCall: call for given regression/classification method, together with 
+#      the associated prediction function and loss evaluation; the 
+#      elements of names(pars) will appear; will make use of 
+#      'dtrn' and 'dtst', and 'comb', the latter being a given 
+#      combinaton of parameter values; see example below
 #   nCombs: number of possible combinations of 'pars'to evaluate; NULL
 #      means all
 #   nTst: desired size of holdout set
@@ -19,11 +22,34 @@
 
 # value:
 
-#   data frame, one column for each element of 'pars', plus 'nTst'
+#   data frame, one column for each element of 'pars', plus 2*'nTst'
 #   columns for the (smoothed) results
+
+# example of 'theCall':
+
+# function() {
+#    rfout <- randomForest(dtrn[,1:5],dtrn[,6],
+#       nodesize=comb[1],maxnodes=comb[2])
+#    preds <- predict(rfout,dtst)
+#    mean(abs(preds - dtst[,6]))
+# }
+
 
 # example
 
-fineTuning <- function(pars,regCall,nCombs=NULL,nTst,nXval) {
+fineTuning <- function(dataset,pars,regCall,nCombs=NULL,nTst,nXval) {
+   # generate the basic output data frame
+   outdf <- expand.grid(pars)
+   if (!is.null(nCombs)) {
+      idxsToKeep <- sample(1:nrow(dataset),nCombs)
+      outdf <- outdf[idxsToKeep,]
+   } 
+   for (xv in 1:nXval) {
+      tstIdxs <- sample(1:nrow(dataset),nTst)
+      assign('dtrn',dataset[-tstIdxs,]
+      assign('dtst',dataset[tstIdxs,]
+
+
+   }
    
 }

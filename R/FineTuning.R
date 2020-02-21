55442edcb848c6f86627bab1a460b84cae314fb3
matloff <matloff@cs.ucdavis.edu>
2020-02-20 22:52:47 -0800

    working!

--- R/FineTuning.R
+++ R/FineTuning.R
@@ -11,8 +11,8 @@
 #      is the set of desired values for the parameter 'x'
 #   theCall: call for given regression/classification method, together with 
 #      the associated prediction function and loss evaluation; the 
-#      elements of names(pars) will appear; will make use of 
-#      'dtrn' and 'dtst', and 'comb', the latter being a given 
+#      elements of names(pars) will appear; args are
+#      'dtrn', 'dtst' and 'comb', the latter being a given 
 #      combinaton of parameter values; see example below
 #   nCombs: number of possible combinations of 'pars'to evaluate; NULL
 #      means all
@@ -34,22 +34,39 @@
 #    mean(abs(preds - dtst[,6]))
 # }
 
-
 # example
 
-fineTuning <- function(dataset,pars,regCall,nCombs=NULL,nTst,nXval) {
+fineTuning <- function(dataset,pars,regCall,nCombs=NULL,nTst=500,nXval,k=NULL) {
    # generate the basic output data frame
    outdf <- expand.grid(pars)
    if (!is.null(nCombs)) {
-      idxsToKeep <- sample(1:nrow(dataset),nCombs)
+      idxsToKeep <- sample(1:nrow(outdf),nCombs)
       outdf <- outdf[idxsToKeep,]
-   } 
-   for (xv in 1:nXval) {
-      tstIdxs <- sample(1:nrow(dataset),nTst)
-      assign('dtrn',dataset[-tstIdxs,]
-      assign('dtst',dataset[tstIdxs,]
-
-
    }
-   
+   meanLoss <- rep(NA,nCombs)
+   losses <- vector(length=nXval)
+   for (combI in 1:nCombs) {
+      for (xv in 1:nXval) {
+         tstIdxs <- sample(1:nrow(dataset),nTst)
+         dtrn <- dataset[-tstIdxs,]
+         dtst <- dataset[tstIdxs,]
+         cmbi <- as.numeric(outdf[combI,])
+         losses[xv] <- regCall(dtrn,dtst,cmbi)
+      }
+      meanLoss[combI] <- mean(losses)
+   }
+   outdf$meanLoss <- meanLoss
+   outdf <- outdf[order(meanLoss),]
+   if (!is.null(k)) {
+      x <- outdf[,1:length(pars)]
+      kout <- kNN(x,meanLoss,x,k)
+      smoothed <- kout$regests
+      outdf$smoothed <- smoothed
+      outdf <- outdf[order(smoothed),]
+   } else outdf <- outdf[order(meanLoss),]
+   row.names(outdf) <- NULL
+   output <- list(outdf=outdf,nTst=nTst,nXval=nXval,k=k)
+   class(output) <- 'tuner'
+   output
 }
+

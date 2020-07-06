a21011044debfe8c0ef82cbd651cc7af57dab502
Norman Matloff <matloff@cs.ucdavis.edu>
2020-07-06 13:47:13 -0700

    update

--- R/Text.R
+++ R/Text.R
@@ -4,39 +4,63 @@
 
 # arguments:
 
-# src:  character string, either a directory name, a file name or a
+# src:  character string, either a directory name or a
 # character vector; cases
 
-#    'dir': each subdirectory represents a class, with each file 
-#    within a subdirectory representing one document from that class;
-#    must be called from the target directory; no other subdirectories
-#    are allowed
-# 
-#    'file': each record contains the text of one document; must be .csv
-# 
+#    'dir': directory; each subdirectory represents a class, with each
+#    file within a subdirectory representing one document from that
+#    class; must be called from the target directory; no other
+#    subdirectories are allowed
+
 #    'vector': each element contains the text of one document
 
+<<<<<<< HEAD
+# labels: taken to the subdirectory names in the 'dir' case, otherwise
+#    supplied by the caller
+=======
 # labels: if src is a file or vector, then vector or factor of class labels
 # text: if src is a file or vector, then character string of document
 #    tests, one element per document
 # hdr: if src is a file, TRUE if file has a header
+>>>>>>> 3a25acef4dbcc0e525900ae2fb5caaea65c8a54c
 # kTop: retain records of only the kTop most-frequent words
 
-textToXY <- function(src='dir',labelPos=NULL,textPos=NULL,hdr=TRUE,kTop=50) 
+textToXY <- function(src='dir',labels=NULL,kTop=50) 
 {
+browser()
    if (src == 'dir') {
       cps <- Corpus(DirSource('.')) 
       labels <- as.factor(list.dirs(recursive=FALSE))
+<<<<<<< HEAD
+   } else cps <- Corpus(VectorSource(src))
+   cps <- preProcessText(cps)
+   tw <- table(w1)
+   two <- tw[order(tw,decreasing=FALSE)]
+   freqWords <- names(two)[1:kTop]
+}
+
+# cps is a tm corpus, returned by e.g. Corpus(); punctuation is removed,
+# etc.; return value is character vector  comprising a word list
+preProcessText <- function(cps) 
+{
+=======
    } else if (src == 'file' || src == 'vector') {
       if (src == 'file') src <- read.csv(src,header=hdr)
       cps <- Corpus(VectorSource(src))
       labels <- as.factor(labels)
    }
+>>>>>>> 3a25acef4dbcc0e525900ae2fb5caaea65c8a54c
 
    cps <- tm_map(cps,tolower)
    cps <- tm_map(cps,removePunctuation)
    cps <- tm_map(cps,removeNumbers)
    cps <- tm_map(cps,removeWords, stopwords("english"))
+<<<<<<< HEAD
+   words <- paste(cps,collapse=' ')
+   strsplit(words,' ')[[1]]
+
+}
+=======
    # cps <- lapply(cps,removeMorePunctuation)
 
    words <- paste(cps,collapse=' ')
@@ -61,3 +85,4 @@ removeMorePunctuation <- function(oneCorpusElt)
    oneCorpusElt
 }
 
+>>>>>>> 3a25acef4dbcc0e525900ae2fb5caaea65c8a54c

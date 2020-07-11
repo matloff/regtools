a2d1ee3a99164e508eb2160000311a7451eb5d5e
matloff <matloff@cs.ucdavis.edu>
2020-07-10 23:26:18 -0700

    done

--- R/Text.R
+++ R/Text.R
@@ -3,7 +3,7 @@
 # preparation for text classification; inputs text, label data; outputs 
 # X matrix, Y vector
 
-############################  ttXY()  ##################################
+############################  textToXY()  ##################################
 
 # wrapper for text2vec pkg
 
@@ -15,7 +15,7 @@
 #      stopWords <- tm::stopwords('english')
 #   kTop: number of most-frequent words to retain
 
-ttXY <- function(docs,labels,kTop=50,stopWords='a') 
+textToXY <- function(docs,labels,kTop=50,stopWords='a') 
 {
    require(text2vec)
 
@@ -26,10 +26,11 @@ ttXY <- function(docs,labels,kTop=50,stopWords='a')
    x <- x[J(x$id)]
 
    # compute vocab, doc term mat
-   prep_fun = tolower
-   tok_fun = word_tokenizer
-   prep_fun = tolower
-   tok_fun = word_tokenizer
+   prep_fun <- tolower
+   tok_fun <- word_tokenizer
+   prep_fun <- tolower
+   tok_fun <- word_tokenizer
+   x$docs <- as.character(x$docs)
    itx <- itoken(x$docs,
         preprocessor = prep_fun,
         tokenizer = tok_fun,
@@ -50,50 +51,50 @@ ttXY <- function(docs,labels,kTop=50,stopWords='a')
    list(x=dtm,y=labels)
 }
 
-##################  textToXY(), to be replaced  ########################
-
-# arguments:
-
-# src:  character string, either a directory name or a
-# character vector; cases
-
-#    'dir': directory; each subdirectory represents a class, with each
-#    file within a subdirectory representing one document from that
-#    class; must be called from the target directory; no other
-#    subdirectories are allowed
-
-#    'vector': each element contains the text of one document
-
-# labels: taken to the subdirectory names in the 'dir' case, otherwise
-#    supplied by the caller
-
-# labels: if src is a file or vector, then vector or factor of class labels
-# text: if src is a file or vector, then character string of document
-#    tests, one element per document
-# kTop: retain records of only the kTop most-frequent words
-
-textToXY <- function(src='dir',labels=NULL,kTop=50) 
-{
-browser()
-   if (inherits(src,'character')) {
-      cps <- Corpus(DirSource('.')) 
-      labels <- as.factor(list.dirs(recursive=FALSE))
-   } else cps <- Corpus(VectorSource(src))
-   words <- preProcessText(cps)
-   tw <- table(words)
-   two <- tw[order(tw,decreasing=FALSE)]
-   freqWords <- names(two)[1:kTop]
-}
-
-# cps is a tm corpus, returned by e.g. Corpus(); punctuation is removed,
-# etc.; return value is character vector  comprising a word list
-preProcessText <- function(cps) 
-{
-   cps <- tm_map(cps,tolower)
-   cps <- tm_map(cps,removePunctuation)
-   cps <- tm_map(cps,removeNumbers)
-   cps <- tm_map(cps,removeWords, stopwords("english"))
-   words <- paste(cps,collapse=' ')
-   strsplit(words,' ')[[1]]
-
-}
+## ##################  textToXY(), tm-based, old  ########################
+## 
+## # arguments:
+## 
+## # src:  character string, either a directory name or a
+## # character vector; cases
+## 
+## #    'dir': directory; each subdirectory represents a class, with each
+## #    file within a subdirectory representing one document from that
+## #    class; must be called from the target directory; no other
+## #    subdirectories are allowed
+## 
+## #    'vector': each element contains the text of one document
+## 
+## # labels: taken to the subdirectory names in the 'dir' case, otherwise
+## #    supplied by the caller
+## 
+## # labels: if src is a file or vector, then vector or factor of class labels
+## # text: if src is a file or vector, then character string of document
+## #    tests, one element per document
+## # kTop: retain records of only the kTop most-frequent words
+## 
+## textToXY <- function(src='dir',labels=NULL,kTop=50) 
+## {
+## browser()
+##    if (inherits(src,'character')) {
+##       cps <- Corpus(DirSource('.')) 
+##       labels <- as.factor(list.dirs(recursive=FALSE))
+##    } else cps <- Corpus(VectorSource(src))
+##    words <- preProcessText(cps)
+##    tw <- table(words)
+##    two <- tw[order(tw,decreasing=FALSE)]
+##    freqWords <- names(two)[1:kTop]
+## }
+## 
+## # cps is a tm corpus, returned by e.g. Corpus(); punctuation is removed,
+## # etc.; return value is character vector  comprising a word list
+## preProcessText <- function(cps) 
+## {
+##    cps <- tm_map(cps,tolower)
+##    cps <- tm_map(cps,removePunctuation)
+##    cps <- tm_map(cps,removeNumbers)
+##    cps <- tm_map(cps,removeWords, stopwords("english"))
+##    words <- paste(cps,collapse=' ')
+##    strsplit(words,' ')[[1]]
+## 
+## }

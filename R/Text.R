776c29c761988aa6260735e062a77a68490ebad1
Norman Matloff <matloff@cs.ucdavis.edu>
2020-07-13 14:48:58 -0700

    forgot to push from home, reconstructed

--- R/Text.R
+++ R/Text.R
@@ -1,12 +1,10 @@
 
 
 
-# preparation for text classification; inputs text, label data; outputs 
-# X matrix, Y vector
-
 ############################  textToXY()  ##############################
 
-# wrapper for text2vec pkg
+# preparation for text classification; inputs text, label data; outputs 
+# X matrix, Y vector
 
 # arguments:
 
@@ -23,15 +21,13 @@ textToXY <- function(docs,labels,kTop=50,stopWords='a')
    # prep
    if (is.null(labels)) labels <- rep(NA,length(docs))
    x <- data.frame(docs,labels,id=1:length(docs))
-   setDT(x)
-   setkey(x,id)
-   # x <- x[J(x$id)]
+   if (!is.character(x$docs)) x$docs <- as.character(x$docs)
+   setDT(x)  # make data frame a by-reference data.table
+   setkey(x,id)  # sort the table by id
 
    # compute vocab, doc term mat
-   prep_fun = tolower
-   tok_fun = word_tokenizer
-   prep_fun = tolower
-   tok_fun = word_tokenizer
+   prep_fun = tolower  # change letters to lower-case
+   tok_fun = word_tokenizer  # break text into words
    itx <- itoken(x$docs,
         preprocessor = prep_fun,
         tokenizer = tok_fun,
@@ -39,16 +35,30 @@ textToXY <- function(docs,labels,kTop=50,stopWords='a')
         progressbar = FALSE)
    vocab <- create_vocabulary(itx)
    vectorizer <- vocab_vectorizer(vocab)
-   dtm <- create_dtm(itx, vectorizer)
+   dtm <- create_dtm(itx, vectorizer)  # document-term matrix, one row per doc
    
    # remove stop words
    vocab <- create_vocabulary(itx, stopwords = stopWords)
    prunedVocab <- prune_vocabulary(vocab)
    vectorizer <- vocab_vectorizer(prunedVocab)
-   dtm <- create_dtm(itx, vectorizer)
+   dtm <- create_dtm(itx, vectorizer)  # new doc-term matrix
+
    nw <- ncol(dtm)
    if (kTop > 0) dtm <- dtm[,(nw-kTop+1):nw]
    dtm <- as.matrix(dtm)
-   list(x=dtm,y=labels)
+   list(x=dtm,y=labels,stopWords=stopWords)
+}
+
+textToXYpred <- function(ttXYout,predDocs) 
+{
+
+   predX <- textToXY(predDocs,NULL,kTop=0,stopWords=ttXYout$stopWords)$x
+   namesTrain <- colnames(ttXYout$x)
+   namesTest <- colnames(predX)
+   x <- matrix(0,nrow=length(predDocs),ncol=length(namesTrain))
+   colnames(x) <- namesTrain
+   for (word in namesTrain) 
+      if (word %in% namesTest) x[,word] <- predX[,word]
+   x
 }
 

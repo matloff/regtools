82f7f313c15e4a5370d16656944b4b150d3fa43c
matloff <matloff@cs.ucdavis.edu>
2020-09-06 22:38:55 -0700

    still need d-1 to d update

--- R/Poly.R
+++ R/Poly.R
@@ -8,17 +8,43 @@
 
 # no goal of efficiency just clarity
 
+# arguments:
+
+#    x: data matrix, each row a data point
+#    deg: ultimate degree of the polynomial version of x
+#    maxInteractDeg: maximum degree of interaction terms
+
+# value:  polynomial version of x, along with an R attribute giving the
+# original column names of x
+
 polyGen <- function(x,deg,maxInteractDeg)
 {
    require(rje)
    if (is.null(colnames(x))) stop('x needs column names')
 
+   # vehicle for the recursion
    worker <- function(p,d)  # all polys of first p vars, degree d
    {
       if (d == 1) {
-         res <- powerSetMat(p)[-1]
-      } else {
-        # need to recurse first on p, then d
+         res <- powerSetMat(p)
+      } else if (p == 1) {
+         res <- rep(1,d+1)
+         for (i in 1:d) res[i+1] <- x^i
+      } else {  
+        # both p,d > 1
+        # will recurse first on p, then d; the first will change the
+        # matrix from p-1 to p columns and new rows, while the 
+        # second will only add more rows
+        res <- NULL
+        tmp <- worker(p-1,d-1)
+        # for each row in tmp, generate the new rows including this new
+        # feature
+        for (i in 1:nrow(tmp)) {
+           rw <- tmp[i,]
+           s <- sum(rw)
+           for (j in 0:(d-1-s)) 
+              res <- rbind(res,c(tmp,j))
+        }
       }
    }
 

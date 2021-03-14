d96978cb013c1bed0f59b2c8bb923542c84ad25c
Norman Matloff <normanmatloff@Normans-MacBook.local>
2021-03-14 15:26:14 -0700

    still need predict.qeNeuralNet

--- R/Quick.R
+++ R/Quick.R
@@ -589,7 +589,7 @@ qeNeuralNet <- function(data,yName,hidden=c(100,100),nEpoch=30,
    holdout=floor(min(1000,0.1*nrow(data))))
 {
    classif <- is.factor(data[[yName]])
-   require(keras)
+   require(neuralnet)
    if (!is.null(holdout)) splitData(holdout,data)
    ycol <- which(names(data) == yName)
    x <- data[,-ycol]
@@ -600,50 +600,44 @@ qeNeuralNet <- function(data,yName,hidden=c(100,100),nEpoch=30,
    y <- data[,ycol]
    if (classif) {
       classNames <- levels(y)
-      yFactor <- y
-      y <- as.numeric(as.factor(y)) - 1
+      linear.output <- FALSE
    } else {
       classNames <- NULL
-      yFactor <- NULL
+      linear.output <- TRUE
    }
-   krsout <- krsFit(x,y,hidden,classif=classif,nClass=length(classNames),
-      nEpoch=nEpoch)
-   krsout$classif <- classif
-   krsout$classNames=classNames
-   krsout$factorsInfo=factorsInfo
-   krsout$x <- x
-   krsout$y <- y
-   krsout$yFactor <- yFactor
-   krsout$trainRow1 <- getRow1(data,yName)
-   class(krsout) <- c('qeNeural',class(krsout))
+   frml <- paste0(yName,' ~ .')
+   data <- as.data.frame(cbind(x,y))
+   names(data)[ncol(data)] <- yName
+   nnout <- neuralnet(frml,data=data,hidden=hidden,linear.output=linear.output,
+      )
+   nnout$classif <- classif
+   nnout$classNames=classNames
+   nnout$factorsInfo=factorsInfo
+   nnout$x <- x
+   nnout$y <- y
+   class(nnout) <- c('qeNeuralNet',class(nnout))
    if (!is.null(holdout)) {
-      predictHoldout(krsout)
-      krsout$holdIdxs <- holdIdxs
+      predictHoldout(nnout)
+      nnout$holdIdxs <- holdIdxs
    }
-   krsout
+   nnout
 }
 
 predict.qeNeuralNet <- function(object,newx=NULL,k=NULL)
 {
    class(object) <- class(object)[-1]
    newx <- setTrainFactors(object,newx)
-   if (nrow(newx) == 1) {  # kludge!; Tensorflow issue
-      kludge1row <- TRUE
-      newx <- rbind(newx,newx)
-   } else kludge1row <- FALSE
    if (!is.null(object$factorsInfo)) {
       newx <- factorsToDummies(newx,omitLast=TRUE,
          factorsInfo=object$factorsInfo)
    }
    preds <- predict(object,newx)
    probs <- attr(preds,'probs')  # may be NULL
-   if (kludge1row) preds <- preds[1]
    if (!object$classif) {
       preds
    } else {
       classNames <- object$classNames
       preds <- classNames[preds+1]
-      if (kludge1row) probs <- probs[1,]
 
       origProbs <- probs
       if (!is.null(k)) {

c72638e5a6ee125cee8189ae05206e689cf82800
Norm Matloff <nsmatloff@ucdavis.edu>
2022-03-27 21:04:05 -0700

    changed on-load msg, tweeked TStoX man pg

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,5 +1,6 @@
 
 .onAttach <- function(libname, pkgname) {
    packageStartupMessage(
-      '\n\n\n\n\n*********************\n\n\n\nLatest version of regtools at GitHub.com/matloff\n\n\nNote: qe*-series soon in a separate pkg, qeML.')
+      '\n\n\n\n\n*********************\n\n\n\nLatest version of regtools at GitHub.com/matloff\n\n\nType ?regtools to see function list by category\n\n\n\n')
 }
+
